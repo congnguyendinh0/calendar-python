dc27d18d337ea97d406987d0ee29d04d0c173087
congnguyendinh0 <congnguyendinh0@gmail.com>
2020-09-11 16:13:41 +0000

    find out if leap year

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import calendar
 
-#print(calendar.weekheader(3))
+
+print(calendar.weekheader(3))
 #print()
 #print(calendar.firstweekday())
-print(calendar.month(2020,9,w=3,l=3))
-print(calendar.monthcalendar(2020,9))
+#print(calendar.month(2020,9,w=3,l=3))
+#print(calendar.monthcalendar(2020,9))
+print(calendar.calendar(2020))
+
+day_of_the_weekday = calendar.weekday(2020,9,11);
+print(day_of_the_weekday)
+
+is_leap = calendar.isleap(2020)
+print(is_leap)
