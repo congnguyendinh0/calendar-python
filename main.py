50aef3e9ea59b02102b1746199cdceea5d03dde5
congnguyendinh0 <congnguyendinh0@gmail.com>
2020-09-11 15:02:34 +0000

    added a months matrix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import calendar
 
-
-print(calendar.weekheader(3))
-print()
-print(calendar.firstweekday())
-print(calendar.month(2020,9))
+#print(calendar.weekheader(3))
+#print()
+#print(calendar.firstweekday())
+print(calendar.month(2020,9,w=3,l=3))
+print(calendar.monthcalendar(2020,9))
